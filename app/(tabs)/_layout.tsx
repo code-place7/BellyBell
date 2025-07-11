94cc46f8c65619abdc0cb206209f9fa695263b7f
MONU MANDAL <monumandel3@gmail.com>
2025-07-11 21:51:03 +0530

    setup appwrite and install necessary packages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,8 +24,8 @@ const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
 );
 
 export default function TabLayout() {
-  const isAuthenticated = false;
-  if (isAuthenticated) return <Redirect href={"/(auth)/sign-in"} />;
+  const isAuthenticated = true;
+  if (isAuthenticated) return <Redirect href={"/(tabs)/index"} />;
   return (
     <Tabs
       screenOptions={{
