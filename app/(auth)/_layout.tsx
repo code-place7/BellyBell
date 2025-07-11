fad63b3331da56d1f359c8720c166d19b12d4aa0
MONU MANDAL <monumandel3@gmail.com>
2025-07-11 21:04:27 +0530

    added tabs and auth folder

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Authlayout = () => {
+  return (
+    <View>
+      <Text>Auth</Text>
+    </View>
+  );
+};
+
+export default Authlayout;
