15aad7822e9cafe6fa7bcaa4c2876b5cc75af50f
MONU MANDAL <monumandel3@gmail.com>
2025-07-12 20:48:53 +0530

    created user collection in appwrite and paste userID in appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   platform: "com.monurix.bellybell",
+  databaseId: "68727b43002d9bb1b0f3",
+  userCollectionId: "68727b9000082141447f",
 };
